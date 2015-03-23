db93a2293f121ecf5c1e2b8a75fad92a45be9e15
jimbojambo63 <jimbo92@tiscali.it>
2015-03-23 01:29:20 +0100

    Update AttrezzoTest.java
    a cosa servono due attrezzi in questo test?

--- diadia/test/diadia/AttrezzoTest.java
+++ diadia/test/diadia/AttrezzoTest.java
@@ -11,21 +11,18 @@ import it.uniroma3.diadia.attrezzi.Attrezzo;
 import org.junit.Before;
 import org.junit.Test;
 
-import it.uniroma3.diadia.attrezzi.*;
-
 public class AttrezzoTest {
 	
-	private Attrezzo cacciavite, martello;
+	private Attrezzo martello;
 	
 	@Before
 	public void setUp(){
-		cacciavite = new Attrezzo("cacciavite", 1);
 		martello = new Attrezzo("martello", 1);
 	}
 	
 	@Test
-	public void testGetPesoCacciavite() {
-		assertEquals(1, cacciavite.getPeso());
+	public void testGetPesoMartello() {
+		assertEquals(1, martello.getPeso());
 	}
 
 	
